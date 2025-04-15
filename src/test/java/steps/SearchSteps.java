76413b6a7a9716986428509b222cae9104d0928d
khalvashi298 <khalvashim2@gmail.com>
2025-04-15 15:50:40 +0400

    Update GitHub Actions workflow with cron job

--- src/test/java/steps/SearchSteps.java
+++ src/test/java/steps/SearchSteps.java
@@ -1,33 +1,33 @@
-package steps;
-
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-
-import java.util.logging.Logger;
-
-import static driver.java.Driver.getDriver;
-
-public class SearchSteps {
-    private static final Logger logger = Logger.getLogger(LoginSteps.class.getName());
-    @Given("the browser is open and the {string} website is loaded")
-    public void theBrowserIsOpenAndTheWebsiteIsLoaded(String url) {
-        logger.info("Opening browser and loading the website: " + url);
-        getDriver().get(url);
-    }
-
-    @Given("the user is on the homepage")
-    public void theUserIsOnTheHomepage() {
-        
-    }
-
-    @When("the user searches for {string}")
-    public void theUserSearchesFor(String arg0) {
-        
-    }
-
-    @Then("search results should be displayed")
-    public void searchResultsShouldBeDisplayed() {
-    }
-}
-   
+//package steps;
+//
+//import io.cucumber.java.en.Given;
+//import io.cucumber.java.en.Then;
+//import io.cucumber.java.en.When;
+//
+//import java.util.logging.Logger;
+//
+//import static driver.java.Driver.getDriver;
+//
+//public class SearchSteps {
+//    private static final Logger logger = Logger.getLogger(LoginSteps.class.getName());
+//    @Given("the browser is open and the {string} website is loaded")
+//    public void theBrowserIsOpenAndTheWebsiteIsLoaded(String url) {
+//        logger.info("Opening browser and loading the website: " + url);
+//        getDriver().get(url);
+//    }
+//
+//    @Given("the user is on the homepage")
+//    public void theUserIsOnTheHomepage() {
+//
+//    }
+//
+//    @When("the user searches for {string}")
+//    public void theUserSearchesFor(String arg0) {
+//
+//    }
+//
+//    @Then("search results should be displayed")
+//    public void searchResultsShouldBeDisplayed() {
+//    }
+//}
+//
--- src/test/resources/features/searchproduct.feature
+++ src/test/resources/features/searchproduct.feature
@@ -1,19 +1,19 @@
-Feature: Search for a product
-
-  Background:
-    Given the browser is open and the "https://dressup.ge/" website is loaded
-    And the user closes the pop-up window
-
-  Scenario: Search for a product by name
-    Given the user is on the homepage
-    When the user searches for "ბრენდი"
-    Then search results should be displayed
-
-#  Scenario: Search for a product by category
+#Feature: Search for a product
+#
+#  Background:
+#    Given the browser is open and the "https://dressup.ge/" website is loaded
+#    And the user closes the pop-up window
+#
+#  Scenario: Search for a product by name
 #    Given the user is on the homepage
-#    When the user searches for "კატეგორია"
+#    When the user searches for "ბრენდი"
 #    Then search results should be displayed
-#Scenario: Search for a product by keyword
-#Given the user is on the homepage
-#When the user searches for "კრემი"
-#Then search results should be displayed
+#
+##  Scenario: Search for a product by category
+##    Given the user is on the homepage
+##    When the user searches for "კატეგორია"
+##    Then search results should be displayed
+##Scenario: Search for a product by keyword
+##Given the user is on the homepage
+##When the user searches for "კრემი"
+##Then search results should be displayed
